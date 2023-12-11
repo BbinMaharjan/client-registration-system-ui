84bcca36d61e394ea70b5051b7f0e865c5590069
Bibin Maharjan <bibin.maharjan@amniltech.com>
2023-12-11 22:18:11 +0545

    feat: :fire: #Remove unwanted code.

--- src/components/form-components/formControl.tsx
+++ src/components/form-components/formControl.tsx
@@ -1,10 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import FormInput from "./formInput";
-import FormMultipleSelect from "./formMultipleSelect";
-import FormRadio from "./formRadio";
 import FormSelect from "./formSelect";
-import FormSwitch from "./formSwitch";
 
 const FormControl: any = (props: any) => {
   const { control, ...rest } = props;
@@ -13,17 +10,8 @@ const FormControl: any = (props: any) => {
     case "input":
       return <FormInput {...rest} />;
 
-    case "radio":
-      return <FormRadio {...rest} />;
-
-    case "multiSelect":
-      return <FormMultipleSelect {...rest} />;
-
     case "select":
       return <FormSelect {...rest} />;
-
-    case "switch":
-      return <FormSwitch {...rest} />;
   }
 };
 
